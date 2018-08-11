4ee2ec8d46e66f10cb5f5e4fe8358609f75e007e
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2018-08-11 21:46:32 +0300

    add snapcraft

--- run_snap.sh
+++ run_snap.sh
@@ -5,4 +5,5 @@ export XLOCALEDIR=$RUNTIME/usr/share/X11/locale
 # Mesa Libs for OpenGL support
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUNTIME/usr/lib/$ARCH/mesa
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUNTIME/usr/lib/$ARCH/mesa-egl
-./LSS
+
+$RUNTIME/bin/LSS
